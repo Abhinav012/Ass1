e3b09243f093b039a0cc8dee59f7dae74a01cc2a
IOS Developer <ios.dev@adeption.io>
2019-12-10 11:45:19 +0530

    first push

---
+++ Assignment1/Assignment1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment1
+//
+//  Created by Thanos on 10/12/19.
+//  Copyright © 2019 Thanos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
